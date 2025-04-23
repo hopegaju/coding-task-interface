152b6d158eddfecc47f9b5f4702ff05c119a86bf
hopegaju <hopegajupr@gmail.com>
2025-04-23 10:41:44 +0200

    PushNotification condole

--- src/PushNotification.java
+++ src/PushNotification.java
@@ -1,2 +1,6 @@
-public class PushNotification {
+public class PushNotification implements NotificationService {
+    @Override
+    public void sendNotification(String message) {
+        System.out.println("Sending Push Notification: " + message);
+    }
 }
