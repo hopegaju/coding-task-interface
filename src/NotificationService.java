dc7ba93993b2b63fe50c80a9bb74ae35e48aaa25
hopegaju <hopegajupr@gmail.com>
2025-04-23 10:14:48 +0200

    classes with functioning methods

--- src/NotificationService.java
+++ src/NotificationService.java
@@ -1,9 +1,15 @@
 public interface NotificationService {
-
+    void sendNotification(String message);
 }
 class EmailNotification implements NotificationService {
-
+    @Override
+    public void sendNotification(String message) {
+        System.out.println("Email Notification");
+    }
 }
 class SmsNotification implements NotificationService {
-
+    @Override
+    public void sendNotification(String message) {
+        System.out.println("SMS Notification");
+    }
 }
