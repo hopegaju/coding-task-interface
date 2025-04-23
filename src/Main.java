800a146800a8bea4c563afca0e3143121f7910ef
hopegaju <hopegajupr@gmail.com>
2025-04-23 10:43:06 +0200

    Added objects and used the methods in the class

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        EmailNotification email= new EmailNotification();
+        SmsNotification sms = new SmsNotification();
+        PushNotification notify = new PushNotification();
+        notify.sendNotification("from Whatsapp");
+        sms.sendNotification("Hello");
+        email.sendNotification("From SheCanCode");
     }
 }
